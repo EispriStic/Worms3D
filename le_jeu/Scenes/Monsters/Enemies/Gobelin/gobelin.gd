a890171b19f2c899952e4eb461f6c92c75959f55
Osteriux <102734647+Osteriux@users.noreply.github.com>
2022-04-19 12:25:42 +0200

    IA 1.0 monstre pas d'attaque

--- le_jeu/Scenes/Monsters/Enemies/Gobelin/gobelin.gd
+++ le_jeu/Scenes/Monsters/Enemies/Gobelin/gobelin.gd
@@ -1,16 +1,66 @@
-extends Node2D
+extends KinematicBody2D
+
+#Statistiques
+var vitality = 1
+var strength = 1
+var defense = 1
+var intelligence = 1
+var luck = 1
+var dexterity = 1
+var speed = 2
+var HP = vitality * 10
+
+var mouvement = Vector2.ZERO
+var direction = 1
+var agro = false
+var player
+var diff
+var gobelin
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
+func _process(delta):
+	gobelin = self.get_position()
+	if agro:
+		chasse(delta)
+#		attaque()
+		agro=false
+	else:
+		patrouille(delta)
+
+func patrouille(delta):
+	if is_on_wall():
+		direction *= -1
+	
+	mouvement.x = Globals.baseSpeed * direction 
+	mouvement.x *= speed * delta
+	
+	mouvement.y = Globals.gravity * delta
+	
+	move_and_slide(mouvement, Vector2.UP)
+
+func chasse(delta):
+	if is_on_wall() and is_on_floor():
+		mouvement.y  = - Globals.jumpForce
+	
+	mouvement.x = Globals.baseSpeed 
+	mouvement.x *= speed * delta
+	
+	mouvement.x *= direction
+	
+	mouvement.y = Globals.gravity * delta
+	
+	move_and_slide(mouvement, Vector2.UP)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_AgroArea_body_entered(body):
+	if body.get_name() == "Player":
+		agro = true
+	player = body.get_position()
+	diff = gobelin.x - player.x
+	if diff > 0:
+		direction = -1
+	elif diff < 0:
+		direction = 1
